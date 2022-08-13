84a92e0e8509c72489c0700718552e641fbdf890
Vitinh-o <victoremanueldsf@gmail.com>
2022-08-12 21:40:40 -0300

    Esta sendo realizado o proceso para finalizar o jogador

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ elemento.addEventListener("click",(m) =>
 { 
     let local = m.target
 
-    let idLocal  = parseInt(m.target.id)
+    let idLocal  = false
     
      
 
@@ -42,20 +42,36 @@ function vencedor(idLocal){
 
 jogadorx.push(idLocal)
 
-let vitoria1 = [1,5,9]
+let vitoria = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 
 
-if(jogadorx === vitoria1)
+for (a=0; a<vitoria.length; a++)
 {
+     let sequencia = vitoria[a] 
 
 
-alert("Vencedor e o x")
+     let pos1 = sequencia[0]
+     let pos2 = sequencia[1]
+     let pos3 = sequencia[3]    
 
+     //console.log(pos1)
+
+    if(jogadorx[pos1] === jogadorx[pos2] && jogadorx[pos1] === jogadorx[pos3])
+    {
+        let vencedorFinal = jogadorx
+        console.log(vencedorFinal)
+
+        if(vencedorFinal == false)
+        {
+            alert("O jogador 1 foi o vencedor")
+        }        
+
+    }
 }
 
-console.log(vitoria1)
-console.log(jogadorx)
+//console.log(vitoria1)
+//console.log(jogadorx)
 
 
 }
