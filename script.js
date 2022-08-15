0b904aa8429828c7530a3f3bc85073311ff80d43
Victor Emanuel de Souza Ferreira <victorem@ccuec.unicamp.br>
2022-08-15 14:28:00 -0300

    Foi adicionado a funcionalidade de identificar vitória do jogador 1. O jogador 2 não funciona ainda.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var a = 0
 
 var i= false
-
-var jogadorx = []
+ 
+var jogadorx = [" "," "," "," "," "," "," "," "," "]
 
 let elemento = document.getElementById("principal")
 
@@ -13,34 +13,38 @@ elemento.addEventListener("click",(m) =>
 { 
     let local = m.target
 
-    let idLocal  = false
+    let idLocal  = m.target.id
     
+    console.log(idLocal)
      
 
     if(!i)
     {
     
-    local.innerHTML = "<img src='./images/pngegg.png' class='estiloX'></img>"
+        local.innerHTML = "<img src='./images/pngegg.png' class='estiloX'></img>"
 
-    i = true
+        i = true
 
-    vencedor(idLocal)
+        vitoria = true
 
     }
     else{
+        
         local.innerHTML = "<img src='./images/pngegg (2).png' class='estiloX'></img>"
+        
         i = false
 
-
+        vitoria = false
+    
     }
 
-
+    vencedor(vitoria, idLocal)
 })
 
 
-function vencedor(idLocal){
+function vencedor(vitorioso, idLocal){
 
-jogadorx.push(idLocal)
+jogadorx[idLocal] = vitorioso
 
 let vitoria = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
@@ -53,19 +57,24 @@ for (a=0; a<vitoria.length; a++)
 
      let pos1 = sequencia[0]
      let pos2 = sequencia[1]
-     let pos3 = sequencia[3]    
+     let pos3 = sequencia[2]    
 
-     //console.log(pos1)
+     console.log(jogadorx)
+    //  console.log(sequencia)
 
-    if(jogadorx[pos1] === jogadorx[pos2] && jogadorx[pos1] === jogadorx[pos3])
+    if(jogadorx[pos1] == jogadorx[pos2] && jogadorx[pos1] == jogadorx[pos3])
     {
-        let vencedorFinal = jogadorx
+        let vencedorFinal = jogadorx[pos1]
         console.log(vencedorFinal)
 
-        if(vencedorFinal == false)
+        if(vencedorFinal == true)
         {
             alert("O jogador 1 foi o vencedor")
         }        
+        else if (vencedor == false)
+        {
+            alert("O jogador 2 foi o vencedor")
+        }
 
     }
 }
