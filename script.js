8170aca88d6338bb910fcb42ae803b877e0104f9
Vitinh-o <victoremanueldsf@gmail.com>
2022-08-18 17:25:18 -0300

    Foi adicionado um recuros para identificar quando o jogo der velha. Alguns outros bugs foram corrigidos

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 var a = 0
-
 var i= false
- 
 var jogadorx = [" "," "," "," "," "," "," "," "," "]
-
 var continuar = true
-
 let elemento = document.getElementById("principal")
 
 
@@ -13,7 +9,6 @@ let elemento = document.getElementById("principal")
 elemento.addEventListener("click",(m) => 
 { 
     let local = m.target
-    
     let idLocal  = m.target.id
 
     //console.log(idLocal)
@@ -51,43 +46,57 @@ elemento.addEventListener("click",(m) =>
     }
 
     vencedor(vitoria, idLocal)
-})
+})  
 
 
 function vencedor(vitorioso, idLocal){
 
-jogadorx[idLocal] = vitorioso
+    jogadorx[idLocal] = vitorioso
 
-let vitoria = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    let vitoria = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    let velha = []
 
-
-
-for (a=0; a<vitoria.length; a++)
-{
-     let sequencia = vitoria[a] 
-
-     let pos1 = sequencia[0]
-     let pos2 = sequencia[1]
-     let pos3 = sequencia[2]    
-
-    if(jogadorx[pos1] == jogadorx[pos2] && jogadorx[pos1] == jogadorx[pos3] && continuar == true)
+    for (a=0; a<vitoria.length; a++)
     {
-        let vencedorFinal = jogadorx[pos1]
+        let sequencia = vitoria[a] 
+        let pos1 = sequencia[0]
+        let pos2 = sequencia[1]
+        let pos3 = sequencia[2]    
 
-        if(vencedorFinal == "x")
+        if(jogadorx[pos1] == jogadorx[pos2] && jogadorx[pos1] == jogadorx[pos3] && continuar == true)
         {
-            setTimeout( ()=> {alert("O jogador 1 foi o vencedor")},100)
-            continuar = false
-        }        
-        else if(vencedorFinal === "o")
-        {
-            setTimeout( ()=> {alert("O jogador 2 foi o vencedor")},100)
-            continuar = false
+            let vencedorFinal = jogadorx[pos1]
+
+            if(vencedorFinal == "x")
+            {
+                setTimeout( ()=> {alert("O jogador 1 foi o vencedor")},100)
+                continuar = false
+            }        
+            else if(vencedorFinal === "o")
+            {
+                setTimeout( ()=> {alert("O jogador 2 foi o vencedor")},100)
+                continuar = false
+            }
+            
+            
         }
-        
-        
     }
-}
+
+    for(jogadas of jogadorx)
+    {
+        if(jogadas != " " )
+        {
+            velha.push(jogadas)
+            console.log(velha)
+
+            if(velha.length == 9 && continuar == true) 
+            {
+                setTimeout( ()=> {alert("O resultado foi velha")},100)
+            }
+        }
+    }
+
+
 }
 
 function resetaPg(){
